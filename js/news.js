db77cce343d1754139f9541840e43520b9c28042
asaduzzaman <eeeshumon@gmail.com>
2022-09-02 23:03:06 +0600

    category api call

--- js/news.js
+++ js/news.js
@@ -1,1 +1,15 @@
-console.log('we have fashion')
+const loadNewsCategory = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayCategory(data.data.news_category);
+}
+
+const displayCategory = categories =>{
+    // console.log(category);
+    for(const category of categories ){
+        console.log(category.category_name);
+    }
+}
+
+loadNewsCategory();
